492d1853a8687c3c2ab51af0891f4bc7ba967643
flystoneflorian <117936610+flystoneflorian@users.noreply.github.com>
2024-02-23 10:01:56 +0100

    Update app.py

--- python/APP/app.py
+++ python/APP/app.py
@@ -47,7 +47,7 @@ async def predictions(input:Item):
     return {'prediction': int(prediction), 'score':(round(score[0],3))
             }
 
-# 5. Run the API with uvicorn
-#    Will run on http://127.0.0.1:8000
+# 5. Heroku dynamic port assignment
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 for local development, use PORT env var on Heroku
+    uvicorn.run(app, host='0.0.0.0', port=port)
